423cf7c90eb716dc6f69add1c6a3c3b802267a36
Luiz Almeida (lhas) <luizhrqas@gmail.com>
2021-07-06 17:05:34 -0300

    finish avatar component

---
+++ src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Avatar } from "./Avatar";
+
+export default {
+  title: "Storybook/Avatar",
+  component: Avatar,
+  argTypes: {
+    src: { control: "text" },
+    name: { control: "text" },
+    hasNotification: { control: "boolean" },
+  },
+} as ComponentMeta<typeof Avatar>;
+
+const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  src: "https://picsum.photos/200",
+  name: "John Doe",
+  hasNotification: false,
+};
---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { useDevice } from "../../hooks/useDevice";
+
+interface AvatarProps {
+  src?: string;
+  name?: string;
+  hasNotification?: boolean;
+}
+
+const Wrapper = styled.button`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  position: relative;
+`;
+
+const Image = styled.img`
+  width: 28px;
+  height: 28px;
+  border-radius: 99px;
+
+  @media (min-width: 768px) {
+    width: 36px;
+    height: 36px;
+  }
+`;
+
+const Badge = styled.span`
+  width: 8px;
+  height: 8px;
+  background: #e72176;
+  border-radius: 8px;
+  display: inline-block;
+  position: absolute;
+  right: -1px;
+  top: -1px;
+`;
+
+export const Avatar = ({ src, name, hasNotification }: AvatarProps) => {
+  const device = useDevice();
+  return (
+    <Wrapper>
+      {hasNotification && device.isMobile() && <Badge />}
+      <Image src={src} alt={name} />
+    </Wrapper>
+  );
+};
