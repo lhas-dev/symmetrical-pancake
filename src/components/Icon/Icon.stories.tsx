2484d989c17fde6cd450dfb57bd0726e35ad1e80
Luiz Almeida (lhas) <luizhrqas@gmail.com>
2021-07-06 16:19:03 -0300

    initialize dynamic Icon component

---
+++ src/components/Icon/Icon.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Icon } from "./Icon";
+
+export default {
+  title: "Storybook/Icon",
+  component: Icon,
+  argTypes: {
+    src: { control: "text" },
+  },
+} as ComponentMeta<typeof Icon>;
+
+const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  src: "DashboardIcon",
+};
---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,30 @@
+import React, { Suspense, useEffect, useState } from "react";
+
+interface IconProps {
+  src: string;
+}
+
+export const Icon = ({ src }: IconProps) => {
+  const dynamicPath = () => import(`../../icons/${src}.svg`);
+  const [path, setPath] = useState("");
+
+  const onReady = async () => {
+    try {
+      const response = await dynamicPath();
+
+      setPath(response.default);
+    } catch (e) {
+      setPath("");
+    }
+  };
+
+  useEffect(() => {
+    onReady();
+  }, [onReady]);
+
+  return (
+    <Suspense fallback={<div />}>
+      <img src={path} alt="Icon" />
+    </Suspense>
+  );
+};
